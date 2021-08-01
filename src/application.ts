c424c6b4b785ba5ca154bd25076b747ecc4c34fe
thofiq <thofiqming@gmail.com>
2021-08-01 20:02:41 +0800

    loopback4 implementation with jwt, mongo

---
+++ src/application.ts
@@ -0,0 +1,53 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+import {AuthenticationComponent} from "@loopback/authentication";
+import {JWTAuthenticationComponent, UserServiceBindings} from "@loopback/authentication-jwt";
+import {MongoDsDataSource} from "./datasources";
+
+export {ApplicationConfig};
+
+export class TestAppApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    //authentication
+    this.component(AuthenticationComponent);
+    this.component(JWTAuthenticationComponent);
+    this.dataSource(MongoDsDataSource, UserServiceBindings.DATASOURCE_NAME);
+
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+}
---
+++ src/sequence.ts
@@ -0,0 +1,5 @@
+import {MiddlewareSequence} from '@loopback/rest';
+
+export class MySequence extends MiddlewareSequence {
+
+}
